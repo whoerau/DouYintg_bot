e4c68dd4b3a682bcc33034e22993277a71f521db
Anonymous <Anonymous>
2024-05-25 23:02:57 +0800

    feat(util): change ua

--- adapter/dlpanda.py
+++ adapter/dlpanda.py
@@ -6,7 +6,7 @@ DLPANDA_XHS_TOKEN = os.getenv('DLPANDA_XHS_TOKEN')
 
 RS = requests.Session()
 RS.headers.update({
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Macddcc; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
 })
 RS.cookies.update({
     'dl_token': DLPANDA_XHS_TOKEN
@@ -15,6 +15,6 @@ RS.cookies.update({
 standalone_chrome = os.getenv('STAND_ALONE_CHROME')
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(
-    'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36')
+    'user-agent=Mozilla/5.0 (Macddcc; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--incognito')
